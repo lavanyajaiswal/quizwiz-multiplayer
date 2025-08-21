bcfaeecfd43f44c50a05167719e42db987683a85
anjalijaiswal04 <anjalijaiswal82479@gmail.com>
2025-08-21 19:28:45 +0530

    Configured socket to use Render backend

--- client/src/socket.js
+++ client/src/socket.js
@@ -3,6 +3,7 @@ import { io } from 'socket.io-client'
 
 export function createSocket(){
   const url = import.meta.env.VITE_SERVER_URL || 'http://localhost:4000'
+ 
   const s = io(url, { autoConnect: true, transports: ['websocket'] })
   return s
 }
